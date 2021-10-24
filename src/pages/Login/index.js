782da44c205ba4ad89ca9f1463db01d4ca7293d6
joelgomezok <joelgomez995@gmail.com>
2021-10-24 16:07:58 -0300

    adding form with formik library

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,55 +1,76 @@
-import { useState } from "react";
-import { useInput } from "../../hooks/useInput";
+import React from 'react';
+import { Formik } from 'formik';
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import styles from "./login.module.scss";
 
 const Login = () => {
   const history = useHistory();
-  const [error, setError] = useState("");
-  const { value: email, bind: bindEmail, reset: resetEmail } = useInput("");
-  const {
-    value: password,
-    bind: bindPassword,
-    reset: resetPassword,
-  } = useInput("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!email) {
-      setError("Please enter your email");
-    } else if (!password) {
-      setError("Please enter your password");
-    } else {
-      const token = await axios.post("http://localhost:8080/login", {
-        email,
-        password,
-      });
-      if (token.data.includes("401")) {
-        setError("You do not have permissions");
-      } else {
-        localStorage.setItem("token", token.data);
-        history.push("/");
-      }
-    }
-    resetEmail();
-    resetPassword();
-  };
   return (
-    <div className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email: challenge@alkemy.org" {...bindEmail}/>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password: react"
-          {...bindPassword}
-        />
-        {error ? <div className={styles.error}>{error}</div> : null}
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+      <div className={styles.container}>
+          <Formik
+             initialValues={{
+                email: '',
+                password:''  
+             }}
+             validate= {(valores) => {
+                let errores = {};
+                if (!valores.email) {
+                      errores.email = "Please enter your email";
+                    } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
+                        errores.email = 'Email can only have letters, numbers, periods, hyphens, and underscores'
+                    }
+                    if (!valores.password) {
+                      errores = "Please enter your password";
+                    } 
+                    return errores ;
+             }}
+             onSubmit={(valores, {resetForm}) => {
+                const tokens = async() =>{
+                    let token = await axios.post("http://localhost:8080/login", {
+                    email: valores.email,
+                    password: valores.password,
+                    }); 
+                    if (token.data.includes("401")) {
+                    alert("You do not have permissions");
+                    } else {
+                    localStorage.setItem("token", token.data);
+                    history.push("/");
+                    }
+                    resetForm();
+                } 
+                tokens();
+             }}
+          >
+              {({ values, errors, touched, handleSubmit, handleChange, handleBlur }) => (
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-4">
+                    <input 
+                      type="email" 
+                      name="email" 
+                      placeholder="Email: challenge@alkemy.org" 
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}  
+                    />
+                    {touched.email && errors.email && <div className={styles.error}>{errors.email}</div>}
+                  </div>
+                  <div className="mb-2">
+                    <input  
+                      type="password" 
+                      name="password" 
+                      placeholder="Password: react" 
+                      value={values.password}
+                      onChange={handleChange} 
+                      onBlur={handleBlur}
+                    />
+                    {touched.password && errors.password && <div className={styles.error}>{errors.password}</div>}
+                  </div>
+                  <button type="submit">Login</button>
+                </form>
+              )}
+          </Formik>
+      </div>
+   );
 };
-
 export default Login;
